da85911eb357b220400f3dd8e1e89c20a0a01483
chriscashen <chris.cashen@hotmail.com>
2023-06-14 18:23:55 -0700

    add layout outlet

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
-    <div>AppLayout</div>
-  )
+    <div>
+      <Header />
+
+      <main>
+        <h1>Content</h1>
+      </main>
+        <Outlet />
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
