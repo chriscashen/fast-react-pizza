56e2a126d11047cff668b3c48145a96d52ced27b
chriscashen <chris.cashen@hotmail.com>
2023-06-23 20:08:25 -0700

    add Button secondary style

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -35,17 +35,17 @@ function Cart() {
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <ul className='divide-y divide-stone-200 border-b mt-3'>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem key={item.key} item={item} />
         ))}
       </ul>
 
-      <div className='mt-6 space-x-2'>
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
