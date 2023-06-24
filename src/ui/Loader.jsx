931114177b6c2f59caad9dcd2c98ff9db042ae3d
chriscashen <chris.cashen@hotmail.com>
2023-06-23 18:14:53 -0700

    add loader modal

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Loader() {
   return (
-    <div className='loader'>Loader</div>
-  )
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
