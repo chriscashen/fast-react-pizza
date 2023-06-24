2f00a16852e22772d73c4ee75cc6846a83b98828
chriscashen <chris.cashen@hotmail.com>
2023-06-23 19:47:16 -0700

    add style MenuItem

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,4 @@
 module.exports = {
-    plugins: [require('prettier-plugin-tailwindcss')],
-    singleQuote: true,
-    max_line_length: 80,
-  
-  }
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+};
