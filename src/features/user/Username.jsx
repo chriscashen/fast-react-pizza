a3c0c8b4f97e8514123e71ce62c3c1ff0c67c423
chriscashen <chris.cashen@hotmail.com>
2023-06-24 06:27:00 -0700

    add username state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Username() {
- const username = useSelector((state) => state.user.username);
+  const username = useSelector((state) => state.user.username);
 
+  if (!username) return null;
 
-  return <div className="hidden text-sm font-semibold md:block">{username}</div>;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
