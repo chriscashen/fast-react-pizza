25a143aa1c2a34f73fa25ee4b598ef037fd03275
chriscashen <chris.cashen@hotmail.com>
2023-06-25 08:51:56 -0700

    add selector in cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,17 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  //   cart: [],
-
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Pizza',
-      quantity: 2,
-      unitPrice: 12,
-      totalPrice: 24,
-    },
-  ],
+    cart: [],
 };
 
 const cartSlice = createSlice({
@@ -42,7 +32,18 @@ const cartSlice = createSlice({
   },
 });
 
-
-export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions;
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((acc, item) => acc + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
