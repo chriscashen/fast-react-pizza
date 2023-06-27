1e0e89db1c8d7bf84183807a285ffa1e03c58243
chriscashen <chris.cashen@hotmail.com>
2023-06-26 19:50:11 -0700

    add update order

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CreateOrder, {action as createOrderAction} from "./features/order/CreateO
 import Order, {loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import {action as updateOrderAction} from "./features/order/UpdateOrder";
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        action: updateOrderAction,
         errorElement: <Error />,
       },
     ],
